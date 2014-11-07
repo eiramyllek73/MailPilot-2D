c56978866087d47cc14595ddc75eb87cbd396152
Kelly McAlpine <eiramyllek73@gmail.com>
2014-11-06 20:46:26 -0500

    Adding CreateJS Boiler Plate

---
+++ MailPilot2D/Scripts/game.js
@@ -0,0 +1,44 @@
+﻿var stage;
+var queue;
+
+function preload()
+{
+    queue = new createjs.LoadQueue();
+    queue.installPlugin(createjs.Sound);
+    queue.addEventListener("complete", init);
+    queue.loadManifest([
+        //{id: "picture", src: "Images/example.jpg"},
+        //{ id: "sound", src: "Sounds/example.mp3" }
+    ]);
+}
+
+function init()
+{
+    stage = new createjs.Stage(document.getElementById);
+    stage.enableMouseOver(20);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    main();
+
+}
+
+function gameLoop(event)
+{
+    stage.update();
+}
+
+var CharacterImage = (function ()
+{
+    function CharacterImage(){
+    }
+    CharacterImage.prototype.update = function () {
+    };
+    return CharacterImage;
+})();
+
+function main()
+{
+    //add code here
+    //some sample code to be replaced
+
+}
